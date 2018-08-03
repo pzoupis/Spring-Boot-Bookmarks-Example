50201b7092fe70b4e515001bf90910566546c7ee
Pantelis Zoupis <pantelis.zoupis@gmail.com>
2018-08-03 21:15:12 +0300

    Add accounts and bookmarks

--- src/main/java/me/pzoupis/bookmarks/BookmarksApplication.java
+++ src/main/java/me/pzoupis/bookmarks/BookmarksApplication.java
@@ -1,12 +1,27 @@
 package me.pzoupis.bookmarks;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookmarksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookmarksApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(AccountRepository accountRepository, BookmarkRepository bookmarkRepository) {
+		return args ->
+			Arrays.asList("jhoeller","dsyer","pwebb","ogierke","rwinch","mfisher","mpollack","jlong")
+				.forEach(username -> {
+					Account account = accountRepository.save(new Account(username, "password"));
+					bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/1/" + username, "A description"));
+					bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/2/" + username, "A description"));
+				});
 	}
 }
